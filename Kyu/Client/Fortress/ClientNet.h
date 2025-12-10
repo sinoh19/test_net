7fdb02949d3ebafc1d3d1dea8b6263cfa256524c
sinoh19 <60386845+sinoh19@users.noreply.github.com>
2025-12-10 10:41:15 +0900

    ㅂㄱ

--- Kyu/Client/Fortress/ClientNet.h
+++ Kyu/Client/Fortress/ClientNet.h
@@ -14,3 +14,4 @@ bool CanControlPlayer(int playerIndex);
 void SendPacket(const char* buf, int len);
 void SendPlayerState(int playerIndex, bool force = false);
 void SendFirePacket(int playerIndex, float startX, float startY, float angle, float power, int shootMode);
+void SendTerrainDelta(int x, int y, int radius, int shootMode);
